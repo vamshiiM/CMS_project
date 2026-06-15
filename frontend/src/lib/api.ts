58b1ec30677b01db746409f408ca7b826fa4985c
vamshi <vmarri92@gmail.com>
2026-06-15 11:05:59 +0530

    changes in api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.MODE === "production" ? import.meta.env.VITE_API_URL : "/api";
+const baseURL = import.meta.env.MODE !== "production" ? import.meta.env.VITE_API_URL : "/api";
 
 export const api = axios.create({
   baseURL: baseURL,
